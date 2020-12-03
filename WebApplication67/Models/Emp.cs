9be764a796a6e4c597032db1fccf508539cd3447
garry1998 <grvcindia34@gmail.com>
2020-12-03 15:58:53 +0530

    Second

---
+++ WebApplication67/Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication67.Models
+{
+    public class Emp
+    {
+        public int Eid { get; set; }
+        public string Ename { get; set; }
+        public string EDesig { get; set; }
+        public DateTime EDoj { get; set; }
+    }
+}
